aec3694d4bb4583356ffbdb285494727f6a6e3c6
Akshitd08 <Akshitd08@gmail.com>
2022-10-17 19:16:13 +0530

    commit

--- index.js
+++ index.js
@@ -14,31 +14,8 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-var coll = document.getElementsByClassName("collapsible1");
-var i;
-var card = document.getElementsByClassName("card1");
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active1");
-    var content = this.previousElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      
-    }
-  });
-}
 
-// //offcanvas
-// function closeOffcanvas(){
-//   offcanvas.style.width = "0px";
-// }
-// var offcanvas = document.getElementById("offcanvas");
-// function openOffcanvas() {
-//     offcanvas.style.width = "250px";
 
-// }
 var Offcanvas = document.getElementById("offcanvas1");
 
 function openOffcanvas() {
